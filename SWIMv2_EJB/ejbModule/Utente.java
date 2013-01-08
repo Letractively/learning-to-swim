400767fa1bf56cd31fb796994311a44f145c9b13
emanuele.uliana.90 <emanuele.uliana.90@6c61fd0f-635b-2080-1e16-552ad7a5131d>
2013-01-08 16:39:09 +0000

    Creazione della tabella UTENTI con Hibernate

---
+++ SWIMv2_EJB/ejbModule/Utente.java
@@ -0,0 +1,65 @@
+import javax.persistence.*;
+
+
+@Entity
+@Table(name="UTENTI")
+public class Utente {
+	@Id
+	@GeneratedValue
+	private Long ID;
+
+	@Column(length = 32)
+	private String Nome;
+	
+	@Column(length = 32)
+	private String Cognome;
+	
+	@Column(length = 32, unique = true)
+	private String Mail;
+	
+	@Column(length = 32)
+	private String Password;
+	
+	
+	public Long getID() {
+		return ID;
+	}
+
+	public void setID(Long iD) {
+		ID = iD;
+	}
+
+	public String getNome() {
+		return Nome;
+	}
+
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+
+	public String getCognome() {
+		return Cognome;
+	}
+
+	public void setCognome(String cognome) {
+		Cognome = cognome;
+	}
+
+	public String getMail() {
+		return Mail;
+	}
+
+	public void setMail(String mail) {
+		Mail = mail;
+	}
+
+	public String getPassword() {
+		return Password;
+	}
+
+	public void setPassword(String password) {
+		Password = password;
+	}
+	
+
+}
