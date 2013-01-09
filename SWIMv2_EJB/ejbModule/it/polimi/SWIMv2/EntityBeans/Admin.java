b50273fe58ddd81044b64b6f1a878e88d092cb0c
walt90 <walt90@hotmail.it>
2013-01-09 17:36:23 +0000

    Creazione altre entità

--- SWIMv2_EJB/ejbModule/it/polimi/SWIMv2/EntityBeans/Admin.java
+++ SWIMv2_EJB/ejbModule/it/polimi/SWIMv2/EntityBeans/Admin.java
@@ -1,11 +1,22 @@
 package it.polimi.SWIMv2.EntityBeans;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.*;
 
+@Entity
 public class Admin extends GenericUser{
 
 	private static final long serialVersionUID = 1L;
 
 	@Column(name = "Admin")
 	private boolean admin = true;
+	
+	@OneToMany(
+			cascade={CascadeType.PERSIST, CascadeType.MERGE},
+            targetEntity=it.polimi.SWIMv2.EntityBeans.Ability.class
+            )
+	private Set<Ability> abilities = new HashSet<Ability>();
+
 }
