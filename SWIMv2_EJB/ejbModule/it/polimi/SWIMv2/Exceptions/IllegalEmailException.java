18fec58836ad740fe5a2362e595e0fb715e200c1
emanuele.uliana.90 <emanuele.uliana.90@gmail.com>
2013-01-11 12:03:26 +0000

    Parsing

---
+++ SWIMv2_EJB/ejbModule/it/polimi/SWIMv2/Exceptions/IllegalEmailException.java
@@ -0,0 +1,10 @@
+package it.polimi.SWIMv2.Exceptions;
+
+public class IllegalEmailException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
--- SWIMv2_EJB/ejbModule/it/polimi/SWIMv2/SessionBeans/RegistrationBeanLocal.java
+++ SWIMv2_EJB/ejbModule/it/polimi/SWIMv2/SessionBeans/RegistrationBeanLocal.java
@@ -1,9 +1,11 @@
 package it.polimi.SWIMv2.SessionBeans;
 
+import it.polimi.SWIMv2.Exceptions.IllegalEmailException;
+
 import javax.ejb.Local;
 
 @Local
 public interface RegistrationBeanLocal {
 
-	public abstract void insertIntoDatabase(String firstName, String lastName, String email, String password, String city);
+	public abstract void insertIntoDatabase(String firstName, String lastName, String email, String password, String city) throws IllegalEmailException;
 }
