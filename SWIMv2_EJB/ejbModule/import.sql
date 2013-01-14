9cca04603de02e0c47f2f81d53468a4e645310a0
emanuele.uliana.90@gmail.com <emanuele.uliana.90@gmail.com@6c61fd0f-635b-2080-1e16-552ad7a5131d>
2013-01-14 16:47:11 +0000

    Creato il file import.sql e fatte alcune modifiche alla classe Admin, senza le quali il login non può funzionare = NON decommentare la relazione con la tabella abilità

---
+++ SWIMv2_EJB/ejbModule/import.sql
@@ -0,0 +1,1 @@
+INSERT INTO User (DTYPE, ID, First_Name, Last_Name, Email, Password, City, Feedback0, Feedback1, Feedback2, Feedback3, Feedback4, Admin) VALUES ('Admin', 1, 'Emanuele', 'Uliana', 'emanuele.uliana.90@gmail.com', '1000:7d5cce6507550ebbd7e0b279a6ab17488f0a3aa6d9f99cb7:4501634f37a09cc1495d30d97bbb37accc2e3bc30d34a137', 'verbania', 0, 0, 0, 0, 0, 1); 
--- SWIMv2_EJB/ejbModule/it/polimi/SWIMv2/EntityBeans/Admin.java
+++ SWIMv2_EJB/ejbModule/it/polimi/SWIMv2/EntityBeans/Admin.java
@@ -13,10 +13,24 @@ public class Admin extends GenericUser{
 	@Column(name = "Admin")
 	private boolean admin = true;
 	
+	public Admin(){}
+	
+	public Admin(String firstName, String lastName, String email, String password, String city, boolean admin){
+		super.setFirstName(firstName);
+		super.setLastName(lastName);
+		super.setEmail(email);
+		super.setPassword(password);
+		super.setCity(city);
+		this.admin = admin;
+		
+	}
+	
+	//CON QUESTA RELAZIONE IL LOGIN NON FUNZIONA, ERGO VA RIPENSATA
+	/*
 	@OneToMany(
 			cascade={CascadeType.PERSIST, CascadeType.MERGE},
             targetEntity=it.polimi.SWIMv2.EntityBeans.Ability.class
             )
-	private Set<Ability> abilities = new HashSet<Ability>();
-
+	private Set<Ability> abilities;
+*/
 }
