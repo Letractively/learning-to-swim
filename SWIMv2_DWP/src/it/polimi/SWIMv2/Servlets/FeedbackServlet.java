69e5f7ede63f0bbf4982c1ec5d5301d0f11c309b
emanuele.uliana.90@gmail.com <emanuele.uliana.90@gmail.com@6c61fd0f-635b-2080-1e16-552ad7a5131d>
2013-01-22 16:19:51 +0000

    Implementato il doPost nella FeedbackServlet

--- SWIMv2_DWP/src/it/polimi/SWIMv2/Servlets/FeedbackServlet.java
+++ SWIMv2_DWP/src/it/polimi/SWIMv2/Servlets/FeedbackServlet.java
@@ -1,6 +1,11 @@
 package it.polimi.SWIMv2.Servlets;
 
+import it.polimi.SWIMv2.SessionBeans.FeedbackBeanLocal;
+
 import java.io.IOException;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,12 +15,42 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class FeedbackServlet
  */
 public class FeedbackServlet extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
+	
+	private InitialContext ctx;
+	private FeedbackBeanLocal fb;
+	
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		try{
+			ctx = new InitialContext();
+			fb = (FeedbackBeanLocal)ctx.lookup("FeedbackBean/local");
+			
+			Integer value = Integer.parseInt(request.getParameter("SOSTITUIRE COL NOME DEL PARAMETRO PRESENTE NELLA PAGINA JSP"));
+			String email = (String)request.getSession().getAttribute("email");
+			
+			boolean feedbackCorrectlyGiven = fb.giveFeedback(email, value);
+			
+			if(feedbackCorrectlyGiven){
+				//TODO forwardare da qualche parte
+			}
+			else{
+				System.out.println("Feedback non aggiunto correttamente");
+			}
+		}
+		catch(NamingException e){
+			e.printStackTrace();
+		}
+		catch(Exception e){
+			System.out.println("Feedback non aggiunto correttamente");
+		}
+		
+		
 		
 	}
 
